ffc266834b1b8ca934cff3bc0f1c17c6c95bbd4b
Jim Jin <jimjin135@gmail.com>
2025-02-13 09:58:37 -0500

    new branch

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -18,10 +18,21 @@ def repeat():
     user_input = request.args.get('input', default='No input provided')  
     return {"body": user_input, "status": 200}
 
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    return {"body": "OK", "status": 200}
+
+@app.route('/hang')
+def hang():
+    while True:
+        time.sleep(1)
+    return {"status": "hanging"}
+
 
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
    # app.run(port=5002)
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="0.0.0.0", port=5002, threaded=False)
